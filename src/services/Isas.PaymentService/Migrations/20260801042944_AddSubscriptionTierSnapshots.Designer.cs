cbb4fff4b029b9d514746ade8173dd3a2cb2b153
Định Trần <dinht1706@gmail.com>
2026-08-01 11:31:56 +0700

    feat(payment): add subscription tier snapshot migration

--- src/services/Isas.PaymentService/Migrations/20260801042944_AddSubscriptionTierSnapshots.Designer.cs
+++ src/services/Isas.PaymentService/Migrations/20260801042944_AddSubscriptionTierSnapshots.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using PaymentService.Models;
@@ -11,9 +12,11 @@ using PaymentService.Models;
 namespace Isas.PaymentService.Migrations
 {
     [DbContext(typeof(PaymentDbContext))]
-    partial class PaymentDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260801042944_AddSubscriptionTierSnapshots")]
+    partial class AddSubscriptionTierSnapshots
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
